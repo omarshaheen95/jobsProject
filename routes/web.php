39cfd52297eab98e21e969f0cdc9262a03fdfe51
DevOmar <devomar095@gmail.com>
2022-05-29 00:13:32 +0300

    Jobs

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'manager'], function () {
+  Route::get('/login', 'ManagerAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'ManagerAuth\LoginController@login');
+  Route::post('/logout', 'ManagerAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'ManagerAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'ManagerAuth\RegisterController@register');
+
+  Route::post('/password/email', 'ManagerAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'ManagerAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'ManagerAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'ManagerAuth\ResetPasswordController@showResetForm');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
